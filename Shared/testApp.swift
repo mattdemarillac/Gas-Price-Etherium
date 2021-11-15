450e0ecc3b8dbe455f5eea975027e2343ed586f7
mattdemarillac <matthewdemarillac@gmail.com>
2021-11-15 18:19:12 +1000

    Initial Commit

---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by matt on 15/11/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
