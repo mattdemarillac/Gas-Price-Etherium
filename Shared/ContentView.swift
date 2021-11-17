63a094ef35e8f1a02b9d93e283f49e12e03b4f82
mattdemarillac <matthewdemarillac@gmail.com>
2021-11-17 10:47:40 +1000

    wip

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,10 +6,50 @@
 //
 
 import SwiftUI
+import UIKit
+
+struct JSONData: Codable {
+    let code: Int
+    var data: GasData
+}
+
+struct GasData: Codable {
+    let rapid: Int
+    let standard: Int
+    let slow: Int
+    let timestamp: Int
+    let priceUSD: Float
+}
 
 struct ContentView: View {
+    @State private var text = "Loading..."
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(self.text)
+            .task {
+                do {
+                    let url = URL(string: "https://etherchain.org/api/gasnow")!
+                    let (data, _) = try await URLSession.shared.data(from: url)
+                    let jsonData = try JSONDecoder().decode(JSONData.self, from: data)
+                    
+                    let fast = String(jsonData.data.rapid).index(String.Index, offsetBy: String.IndexDistance, limitedBy: <#T##String.Index#>)
+                    
+                    let standard = String(jsonData.data.standard).index(String.Index, offsetBy: String.IndexDistance, limitedBy: <#T##String.Index#>)
+                    
+                    let slow = String(jsonData.data.slow).index(String.Index, offsetBy: String.IndexDistance, limitedBy: <#T##String.Index#>)
+                    
+                    let etherPrice = jsonData.data.priceUSD
+                    
+                    text =
+                        "Fast: " + fast + " GWei" +
+                        "\nStandard: " + standard + " GWei" +
+                        "\nSlow: " + slow + " GWei"
+                } catch {
+                    print(error)
+                    text = "Error"
+                }
+                
+            }
             .padding()
     }
 }
@@ -17,5 +57,21 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension String {
+    // formatting text for currency textField
+    func currencyFormatting() -> String {
+        if let value = Double(self) {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .currency
+            formatter.maximumFractionDigits = 2
+            formatter.minimumFractionDigits = 2
+            if let str = formatter.string(for: value) {
+                return str
+            }
+        }
+        return ""
     }
 }
